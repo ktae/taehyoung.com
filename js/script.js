6feb4c5bd3df82c9258574e0c6db7bd425db1436
Taehyoung Kim <me@taehyoung.com>
2016-12-20 18:35:29 -0600

    new style

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// reset scroll position
+window.onbeforeunload = function(){
+    window.scrollTo(0,0);
+}
+
+// All the rest
+document.addEventListener('DOMContentLoaded', function(){
+
+    // Fetch and then rest the content of the logo
+    var logo = document.querySelector('.logo');
+
+    // Array content
+    var titles = ['Student', 'Developer', 'Minimalist'];
+    var title = titles[Math.floor(Math.random()*titles.length)];
+
+    // Initialize the typewriter
+    var typewriter = require('typewriter');
+    var tw = typewriter(document.querySelector('.logo')).withAccuracy(100)
+                                                        .withMinimumSpeed(13)
+                                                        .withMaximumSpeed(18)
+                                                        .build();
+    // Write that header
+    tw.wait(1000)
+        .type('Taehyoung Kim.')
+        .wait(600)
+        .type(' ' + title , function(){
+            document.querySelector('body').classList.add('active');
+            document.querySelector('.content').classList.add('active');
+        });
+
+});
